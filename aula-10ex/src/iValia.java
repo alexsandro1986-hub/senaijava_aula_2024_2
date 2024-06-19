c68f8e8ffbee91859b8b7dd5eeef5dbafc62c3db
ALEXSANDRO SILVA SILVA <alexsandro_silva16@estudante.sesisenai.org.br>
2024-06-19 16:06:07 -0300

    ex_final

--- aula-10ex/src/iValia.java
+++ aula-10ex/src/iValia.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
-public class iValia  extends JFrame {
+public class iValia extends JFrame {
     private ArrayList<Avaliacao> alzheimer = new ArrayList();
     //Avaliacao a = new Avaliacao("Forno", "Pizza", 2, "Nâo vale a pena");
     public JPanel painelPrincipal;
@@ -18,23 +18,17 @@ public class iValia  extends JFrame {
     private JButton lblMostrar;
     private JButton lblFake;
 
+     //boatao mostrar tudo
     private void mostrarTudo() {
         String s = "";
         for (Avaliacao e : alzheimer) {
             s += String.format("Estabelecimento: %s \nItens: %s \nNota: %d \nComentario: %s\n---------\n",
                     e.getEstabelecimento(), e.getItens(), e.getNota(), e.getComentario());
         }
-        txttexto.setText(s);
-    }
+        txttexto.setText(s);//mostra no espaçp em branco
+        //boatao mostrar tudo
 
-    public iValia() {
-
-        Avaliacao Forno = new Avaliacao("Forno","Pizza",1,"Não vale a pena");
-//        Avaliacao Torta = new Avaliacao;
-//        Avaliacao Cuca = new Avaliacao();
-        alzheimer.add(Forno);
-
-
+        //boatao guardar
         lblGuardar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,20 +43,16 @@ public class iValia  extends JFrame {
                         "Produto: " + txtestabe.getText() +
                                 "\nitens:" + txtitens.getText() +
                                 "\nnotas:" + txtnotas.getText() +
-                        "\ncomentarios:" + txtcomentario.getText());
-                txtNome.setText("");
-                txtValor.setText("");
-                txtpeso.setText("");
+                                "\ncomentarios:" + txtcomentario.getText());
+                txtestabe.setText("");
+                txtitens.setText("");
+                txtnotas.setText("");
+                txtcomentario.setText("");
             }
         });
+        //boatao guardar
 
-        lblMostrar.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mostrarTudo();
-            }
-        });
-
+        //boatao buscar estabelecimento
         lblBuscarEs.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -86,8 +76,72 @@ public class iValia  extends JFrame {
                 JOptionPane.showMessageDialog(null, "Produto não encontrado.");
             }
         });
+        //boatao buscar estabelecimento
+
+
+        //botao buscar item
+    lblBuscarItem.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // Obter o nome a ser localizado
+            String nomeItem = JOptionPane.showInputDialog("Digite o nome do Item:");
+
+            // Procurar o produto na lista de dadosProducao
+            for (Avaliacao produto : alzheimer) {
+                if (produto.getItens().equalsIgnoreCase(nomeItem)) {
+                    // Se o produto for encontrado, exibir seus dados
+                    JOptionPane.showMessageDialog(null,
+                            "Produto encontrado:\n" +
+                                    "Itens: " + produto.getItens() + "\n" +
+                                    "Nome: " + produto.getEstabelecimento() + "\n" +
+                                    "Nota: " + produto.getNota());
+                    return; // Encerrar a busca após encontrar o produto
+                }
+            }
+
+            // Se o produto não for encontrado, exibir uma mensagem de erro
+            JOptionPane.showMessageDialog(null, "Produto não encontrado.");
+        }
+    });
+
+        //botao buscar item
+
 
     }
+
+    public iValia() {
+
+        Avaliacao Forno = new Avaliacao("Forno", "Pizza", 1, "Não vale a pena");
+        Avaliacao VilaAcoriana = new Avaliacao("Vila Açoriana", "Pizza", 9,"Muito boa");
+        Avaliacao MS_Salgado = new Avaliacao("Ms Salgado", "Salgado", 9,"Muito bom");
+        Avaliacao PizzaDobro = new Avaliacao("Pizza em dobro", "Pizza", 9,"satisfátoria");
+        alzheimer.add(Forno);
+
+        //botãoto fakes
+     lblFake.addActionListener(new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            alzheimer.add(VilaAcoriana);
+            alzheimer.add(MS_Salgado);
+            alzheimer.add(PizzaDobro);
+            mostrarTudo();
+        }
+    });
+    //boãoto fakes
+
+
+//boatao mostrar tudo
+        lblMostrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mostrarTudo();
+            }
+        });
+        //boatao mostrar tudo
+
+
+    }
+
 }
 
 
