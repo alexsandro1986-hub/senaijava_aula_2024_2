98e4d1bc95bbf4686b066469fd826ee7b0bed975
ALEXSANDRO SILVA SILVA <alexsandro_silva16@estudante.sesisenai.org.br>
2024-06-20 16:56:38 -0300

    milk

--- Milkshakespeare/src/Loja.java
+++ Milkshakespeare/src/Loja.java
@@ -1,5 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+import java.awt.event.ActionListener;
 
 public class Loja  extends JFrame {
     private ArrayList<Pedido> sequencia = new ArrayList();
@@ -12,46 +15,69 @@ public class Loja  extends JFrame {
     private JTextField pedidoa;
     private JTextArea txttexto;
 
-    /*  if (p instanceof Funcionario) {
-              Funcionario f = (Funcionario) p;
-              System.out.println(f.getCetor());
-          } else if (p.getClass() == Professor.class) {
-              Professor profe = (Professor) p;
-              System.out.println(profe.getArea());
-          }*/
+    public Loja() {
+        btnMilk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int id = 1;
+                int numero = 2;
+                String tamanho = "pequeno";
+                String sabor = "choclo";
+
+                Milkshake novoMilk = new Milkshake(id, numero, tamanho, sabor);
+                sequencia.add(novoMilk);
+                mostrarTudo();  // Atualiza a exibição dos pedidos
+
+                JOptionPane.showMessageDialog(null,
+                        "ID: "+ sequencia.size() + 1+
+                                "\nNúmero: " + numero +
+                                "\nNome: " + tamanho+
+                                "\nNome: " + sabor,
+                        "Informações do Livro",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });//boatao mostrar Milk
+
+        //boatao mostrar livro
+        btnLivro.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed (ActionEvent e){
+                int id = 1;
+                int numero = 2;
+                String nome = "teste";
+
+                livro novoLivro = new livro(id, numero, nome);
+                sequencia.add(novoLivro);
+                mostrarTudo();  // Atualiza a exibição dos pedidos
+
+                JOptionPane.showMessageDialog(null,
+                        "ID: "+ sequencia.size() + 1+
+                                "\nNúmero: " + numero +
+                                "\nNome: " + nome,
+                        "Informações do Livro",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });//boatao mostrar livro
+    }
+
     private void mostrarTudo() {
         String s = "";
         for (Pedido e : sequencia) {
             if (e instanceof livro ){
                 livro l = (livro) e;
-                
-                s += String.format("Estabelecimento: %s \nItens: %s \nNota: %d \nComentario: %s\n---------\n",
+
+                s += String.format("Estabelecimento:  \nid: %d \nnumero: %d \nComentario: %s\n---------\n",
                         e.getId(), e.getNumero(),l.getNome());
             } else if (e instanceof Milkshake) {
                 Milkshake m = (Milkshake) e;
-                s += String.format("Estabelecimento: %s \nItens: %s \nNota: %d \nComentario: %s\n---------\n",
+                s += String.format("Estabelecimento:  \nid: %s \nnumero: %d \ntamanho: %s \nsabor: %s \n---------\n",
                         e.getId(), e.getNumero(),m.getSabor(),m.getTamanho());
             }
 
         }
         txttexto.setText(s);
-
-   /* private static void testStringsNaturalOrdering() {
-        Queue<String> testStringsPQ = new PriorityQueue<>();
-        testStringsPQ.add("abcd");
-        testStringsPQ.add("1234");
-        testStringsPQ.add("23bc");
-        testStringsPQ.add("zzxx");
-        testStringsPQ.add("abxy");
-
-        System.out.println("Strings armazenadas por ordenamento natural em uma fila de prioridades\n");
-        while (!testStringsPQ.isEmpty()) {
-            System.out.println(testStringsPQ.poll());
-        }
-    }*/
-
-
     }
+
 }
 
 
